316e4234ddf5351a29b2ad66d62296b6afedd0cb
Cristian Negulescu <crnegule@microsoft.com>
2020-01-28 13:17:58 +0000

    V2.4 ps v4 issue
    Solved running issues under powershell 4

--- LogCatcher/General/CatchFilteredIISzip.ps1
+++ LogCatcher/General/CatchFilteredIISzip.ps1
@@ -10,48 +10,48 @@ function CatchFilteredIISzip {
     $Global:FilteredZipFile = $ZipOutput + "\LOGS-" + $date + ".zip"
     If (Test-path $FilteredZipFile) { Remove-item $FilteredZipFile -Force } 
     $GeneralTempLocation = $FilteredTempLocation + "\General"
-$SiteTempLocation = $FilteredTempLocation + "\Sites"
-foreach ($FilteredLogDefinition in $FilteredLOGSDefinitions) {
-    if ($FilteredLogDefinition.Level -eq 'Site')
- {
-        if ($FilteredLogDefinition.Product -eq "SitePath" ) {
-            $idFloder = $SiteTempLocation + "\" + $FilteredLogDefinition.LogName
+    $SiteTempLocation = $FilteredTempLocation + "\Sites"
+    foreach ($FilteredLogDefinition in $FilteredLOGSDefinitions) {
+        if ($FilteredLogDefinition.Level -eq 'Site') {
+            if ($FilteredLogDefinition.Product -eq "SitePath" ) {
+                $idFloder = $SiteTempLocation + "\" + $FilteredLogDefinition.LogName
             
-            new-item -Path $SiteTempLocation -ItemType "directory" -Name $FilteredLogDefinition.LogName -ErrorAction silentlycontinue -ErrorVariable +ErrorMessages | Out-Null
-            Robocopy.exe $FilteredLogDefinition.Location $idFloder *.config /s | Out-Null
-        }
-        elseif ($FilteredLogDefinition.Product -eq "FrebLogs" ){
-            $idFloder = $SiteTempLocation + "\" + $FilteredLogDefinition.LogName
-            $SiteLogs = $idFloder + "\FrebLogs"
-            Robocopy.exe $FilteredLogDefinition.Location $SiteLogs /s /maxage:$MaxDays | Out-Null
-        }
-        else {
-            $idFloder = $SiteTempLocation + "\" + $FilteredLogDefinition.LogName
-            $SiteLogs = $idFloder + "\IISLogs"
-            Robocopy.exe $FilteredLogDefinition.Location $SiteLogs /s /maxage:$MaxDays | Out-Null
-        }
- 
-    }
-    else{
-        if( $FilteredLogDefinition.TypeInfo -eq "Folder" ){
-            if( $FilteredLogDefinition.LogName -eq "HTTPERRLog" ){
-                $httperr = $GeneralTempLocation+"\HttpERR"
-                Robocopy.exe $FilteredLogDefinition.Location $httperr /s | Out-Null
+                new-item -Path $SiteTempLocation -ItemType "directory" -Name $FilteredLogDefinition.LogName -ErrorAction silentlycontinue -ErrorVariable +ErrorMessages | Out-Null
+                Robocopy.exe $FilteredLogDefinition.Location $idFloder *.config /s | Out-Null
             }
-            elseif( $FilteredLogDefinition.LogName -eq "IISConfig" ){
-                $IISConfig = $GeneralTempLocation+"\IISConfig"
-            Robocopy.exe $FilteredLogDefinition.Location $IISConfig *.config /s | Out-Null
-        }
-            else{
-                $NETFramework = $GeneralTempLocation+"\NETFramework"
-            Robocopy.exe $FilteredLogDefinition.Location $NETFramework *.config /s | Out-Null
-        }}
-        else {
-            Copy-Item -Path $FilteredLogDefinition.Location -Destination $GeneralTempLocation -Recurse -Force -ErrorAction silentlycontinue -ErrorVariable +ErrorMessages
+            elseif ($FilteredLogDefinition.Product -eq "FrebLogs" ) {
+                $idFloder = $SiteTempLocation + "\" + $FilteredLogDefinition.LogName
+                $SiteLogs = $idFloder + "\FrebLogs"
+                Robocopy.exe $FilteredLogDefinition.Location $SiteLogs /s /maxage:$MaxDays | Out-Null
+            }
+            else {
+                $idFloder = $SiteTempLocation + "\" + $FilteredLogDefinition.LogName
+                $SiteLogs = $idFloder + "\IISLogs"
+                Robocopy.exe $FilteredLogDefinition.Location $SiteLogs /s /maxage:$MaxDays | Out-Null
+            }
  
         }
+        else {
+            if ( $FilteredLogDefinition.TypeInfo -eq "Folder" ) {
+                if ( $FilteredLogDefinition.LogName -eq "HTTPERRLog" ) {
+                    $httperr = $GeneralTempLocation + "\HttpERR"
+                    Robocopy.exe $FilteredLogDefinition.Location $httperr /s | Out-Null
+                }
+                elseif ( $FilteredLogDefinition.LogName -eq "IISConfig" ) {
+                    $IISConfig = $GeneralTempLocation + "\IISConfig"
+                    Robocopy.exe $FilteredLogDefinition.Location $IISConfig *.config /s | Out-Null
+                }
+                else {
+                    $NETFramework = $GeneralTempLocation + "\NETFramework"
+                    Robocopy.exe $FilteredLogDefinition.Location $NETFramework *.config /s | Out-Null
+                }
+            }
+            else {
+                Copy-Item -Path $FilteredLogDefinition.Location -Destination $GeneralTempLocation -Recurse -Force -ErrorAction silentlycontinue -ErrorVariable +ErrorMessages
+ 
+            }
+        }
     }
- }
 
     $ExcludeFilter = @()
     $Errlog = "HTTP*"
@@ -60,31 +60,37 @@ foreach ($FilteredLogDefinition in $FilteredLOGSDefinitions) {
         $stringtoADD = "*" + $id
         $ExcludeFilter += $stringtoADD
     }
-   $iisInfo = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\InetStp\
-    IF ($iisInfo.MajorVersion -ge 8)
-    {
-    GenerateSiteOverview -ErrorAction silentlycontinue -ErrorVariable +ErrorMessages | Out-Null
-    $logName = $GeneralTempLocation+"\SiteOverview.csv"
-    $Global:SiteOverview | Export-csv -Path $logName -NoTypeInformation -Force -ErrorAction silentlycontinue -ErrorVariable +ErrorMessages
-    Add-Type -assembly "system.io.compression.filesystem"
-    [io.compression.zipfile]::CreateFromDirectory($FilteredTempLocation, $FilteredZipFile) 
-    
-  Remove-Item -Recurse $FilteredTempLocation -Force -ErrorAction silentlycontinue -ErrorVariable +ErrorMessages    
-}
-    else {
-        if($Host.Version.Major -ge 3)
-        {  
-             Add-Type -assembly "system.io.compression.filesystem"
+    $iisInfo = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\InetStp\
+    IF ($iisInfo.MajorVersion -ge 8) {
+        if ($Host.Version.Major -ge 5) {
+            GenerateSiteOverview -ErrorAction silentlycontinue -ErrorVariable +ErrorMessages | Out-Null
+            $logName = $GeneralTempLocation + "\SiteOverview.csv"
+            $Global:SiteOverview | Export-csv -Path $logName -NoTypeInformation -Force -ErrorAction silentlycontinue -ErrorVariable +ErrorMessages
+            Add-Type -assembly "system.io.compression.filesystem"
             [io.compression.zipfile]::CreateFromDirectory($FilteredTempLocation, $FilteredZipFile) 
-          Remove-Item -Recurse $FilteredTempLocation -Force -ErrorAction silentlycontinue -ErrorVariable +ErrorMessages   
-          "$Time Exception Message: IIS server version is lower than 8.0 so no SiteOverView generated!" | Out-File $ToolLog -Append
+    
+            Remove-Item -Recurse $FilteredTempLocation -Force -ErrorAction silentlycontinue -ErrorVariable +ErrorMessages    
+        }
+        else {
+            
+            Add-Type -assembly "system.io.compression.filesystem"
+            [io.compression.zipfile]::CreateFromDirectory($FilteredTempLocation, $FilteredZipFile) 
+            Remove-Item -Recurse $FilteredTempLocation -Force -ErrorAction silentlycontinue -ErrorVariable +ErrorMessages 
+        }
+    }
+    else {
+        if ($Host.Version.Major -ge 3) {  
+            Add-Type -assembly "system.io.compression.filesystem"
+            [io.compression.zipfile]::CreateFromDirectory($FilteredTempLocation, $FilteredZipFile) 
+            Remove-Item -Recurse $FilteredTempLocation -Force -ErrorAction silentlycontinue -ErrorVariable +ErrorMessages   
+            "$Time Exception Message: IIS server version is lower than 8.0 so no SiteOverView generated!" | Out-File $ToolLog -Append
 
         }
 
-        else{
-        "$Time Exception Message: IIS server version is lower than 8.0 so no SiteOverView generated!" | Out-File $ToolLog -Append
-        "$Time Exception Message: Zip was not created as system.io.compression.filesystem version could not be loaded!" | Out-File $ToolLog -Append
-    }
+        else {
+            "$Time Exception Message: IIS server version is lower than 8.0 so no SiteOverView generated!" | Out-File $ToolLog -Append
+            "$Time Exception Message: Zip was not created as system.io.compression.filesystem version could not be loaded!" | Out-File $ToolLog -Append
+        }
     }
 
     Foreach ($Message in $ErrorMessages) {
