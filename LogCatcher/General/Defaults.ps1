a185e38e63a7050af9e1349d2406762aefdfb329
Sofia Estrela <sofiam.estrela@gmail.com>
2022-07-20 12:15:22 +0100

    Issue 16
    Force freb collection

--- LogCatcher/General/Defaults.ps1
+++ LogCatcher/General/Defaults.ps1
@@ -5,6 +5,7 @@ $Global:ToolLog = "$scriptPath\ScriptLog\ToolLog.log"
 $Global:ZipOutput = "$scriptPath" #if you want to revert to Original replace with : $Global:ZipOutput = "$scriptPath"
 $Global:DefaultMaxDays = "2"
 $Global:CurentSites = @()
+$Global:ForceFREBCollection = $false
 $sitesInfo = Get-Website | Sort-Object -Property id
 foreach ($siteinfo in $sitesInfo)
 { $Global:CurentSites += $siteinfo.id }
